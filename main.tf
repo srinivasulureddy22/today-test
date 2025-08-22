7a495751170aed0176f74edac9d8a357a42d8923
srinivasulureddy22 <61047715+srinivasulureddy22@users.noreply.github.com>
2025-08-22 12:25:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,41 +2,20 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "~> 4.8.0"
     }
   }
+  required_version = ">=1.9.0"
 }
 
 provider "azurerm" {
-  features {}
-}
+  features {
 
-resource "random_string" "resource_code" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
-resource "azurerm_resource_group" "tfstate" {
-  name     = "tfstate"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.resource_code.result}"
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                 = azurerm_resource_group.tfstate.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  allow_nested_items_to_be_public = false
-
-  tags = {
-    environment = "staging"
   }
+
 }
 
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_id    = azurerm_storage_account.tfstate.id
-  container_access_type = "private"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "UK South"
 }
