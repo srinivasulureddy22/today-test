e09cdee69084aaa8d8419017951df0d9d66f369d
srinivasulureddy22 <61047715+srinivasulureddy22@users.noreply.github.com>
2025-08-22 13:04:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,22 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.8.0"
-    }
-  }
-  required_version = ">=1.9.0"
-}
-
 provider "azurerm" {
-  features {
-
-  }
-
+  features {}
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "UK South"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["172.16.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["172.16.1.0/24"]
 }
