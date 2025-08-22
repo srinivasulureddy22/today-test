b11c8088b9806e5e96a31884a1303af37787ce3e
srinivasulureddy22 <61047715+srinivasulureddy22@users.noreply.github.com>
2025-08-22 14:50:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,21 @@
+terraform {
+  required_version = ">=1.9.0"
+}
+
 provider "azurerm" {
   features {}
+  use_cli = true   # Reuse Azure CLI login from Azure DevOps pipeline
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "UK South"
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-demo-rg"
+  location = "East US"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["172.16.0.0/16"]
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["172.16.1.0/24"]
+resource "azurerm_storage_account" "sa" {
+  name                     = "tfdemostorageacct123"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
