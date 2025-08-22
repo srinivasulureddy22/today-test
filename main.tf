56f716944ea4542b9f163b35cabfe2d6c82c6a3d
srinivasulureddy22 <61047715+srinivasulureddy22@users.noreply.github.com>
2025-08-22 11:01:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.8.0"
+    }
+  }
+  required_version = ">=1.3.7"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "example_rg" {
+  name     = "storagerg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "ex_storage" {
+  name                     = "cloudtechstorage"
+  resource_group_name      = azurerm_resource_group.example_rg.name
+  location                 = azurerm_resource_group.example_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
